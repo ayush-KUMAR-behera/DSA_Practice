f9359fe81c2d39cc41f638881bfacb7aa8438a5c
ayush-KUMAR-behera <ayushkumarbehera0237@gmail.com>
2026-04-28 15:21:45 +0530

    added MinSizeSubArrSum

---
+++ Sliding_Window/MinSizeSubArrSum.java
@@ -0,0 +1,36 @@
+package Sliding_Window;
+
+//Problem: Minimum Size Subarray Sum
+//Time: O(n)
+//Space: O(n)
+
+public class MinSizeSubArrSum {
+    public static void main(String[] args) {
+        int[] arr={2,4,1,2,3};
+        int target=7;
+        System.out.println(minLength(arr, target));
+
+    }
+    public static int minLength(int[] arr,int target){
+        int high=0;
+        int low=0;
+        int res=Integer.MAX_VALUE;
+        int sum=0;
+        while(high<arr.length){
+            sum=sum+arr[high];
+            while(sum>=target){
+                int len=high-low+1;
+                if(len<res){
+                    res=len;
+                }
+                sum=sum-arr[low];
+                low++;
+            }
+            high++;
+        }
+        if(res==Integer.MAX_VALUE){
+            return 0;
+        }
+        return res;
+    }
+}
