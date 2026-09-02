100d7fe92ecac0dc8cfe149bdc5df8e291fe907b
ayush-KUMAR-behera <ayushkumarbehera0237@gmail.com>
2026-09-02 09:50:20 +0530

    Learn  Binary Search

---
+++ BInary_Search/Binary_Search.java
@@ -0,0 +1,24 @@
+package BInary_Search;
+
+public class Binary_Search {
+    public static void main(String[] args) {
+        int[] arr={-1,0,3,5,9,12};
+        int target=9;
+        System.out.println(search(arr, target));
+    }
+       public static int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+}
